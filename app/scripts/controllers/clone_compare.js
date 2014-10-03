b17318a71515213a58a6a3ffaf268135d1bdd985
Aaron Rosenfeld <ar374@drexel.edu>
2014-10-03 14:12:47 -0400

    ugly clone comparison canvas

--- app/scripts/controllers/clone_compare.js
+++ app/scripts/controllers/clone_compare.js
@@ -27,13 +27,18 @@
                 'A', 'GCA': 'A', 'GCG': 'A', 'GAT': 'D', 'GAC': 'D', 'GAA': 'E',
                 'GAG': 'E', 'GGT': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G' };
 
+                var aaLookup = function(nt) {
+                    if (nt in aa_lookup) {
+                        return aa_lookup[nt];
+                    }
+                    return null;
+                }
 
                 var drawCompare = function(cloneId) {
-                    var TOP_PAD = 10;
+                    var TOP_PAD = 30;
                     var LEFT_PAD = 5;
-                    var CHAR_SPACE = 8;
+                    var CHAR_SPACE = 20;
                     var V_PER_SEQ = 20;
-                    var V_SPACE = 20;
 
                     var canvas = $('#compare_' + cloneId).get(0);
                     var ctx = canvas.getContext('2d');
@@ -48,40 +53,152 @@
 
                     canvas.width = (labelMaxLength +
                         info.clone.germline.length) * (CHAR_SPACE + .01);
-                    canvas.height = (info.seqs.length + 2) * V_PER_SEQ;
-                    ctx.font = 'bold 10px Courier New';
+                    canvas.height = (info.seqs.length + 2) * V_PER_SEQ + 35;
+                    ctx.font = 'bold 12px Courier New';
 
                     ctx.fillText('Germline', LEFT_PAD, TOP_PAD);
+                    var middlePad = (CHAR_SPACE - 12) * labelMaxLength;
                     angular.forEach(info.clone.germline, function(c, i) {
-                        var nt = info.clone.germline.substring(i, i + 3);
-                        if (nt in aa_lookup) {
-                            ctx.fillStyle = lookup[aa_lookup[nt]];
-                        } else {
-                            ctx.fillStyle = '#000000';
-                        }
-                        ctx.fillText(c,
-                            LEFT_PAD + (7 * labelMaxLength) + (8 * i),
-                            10);
-                    });
-
-                    var i = 0;
-                    angular.forEach(info.seqs, function(seq, id) {
-                        var label = seq.sample.id + ': ' + seq.seq_id;
-                        ctx.fillText(label, LEFT_PAD, TOP_PAD + V_SPACE * (1 +
-                        i));
-                        angular.forEach(seq.sequence, function(c, j) {
-                            var nt = seq.sequence.substring(j, j + 3);
+                        if (i % 3 == 0) {
+                            var nt = info.clone.germline.substring(i, i + 3);
                             if (nt in aa_lookup) {
-                                ctx.fillStyle = lookup[aa_lookup[nt]];
+                                ctx.fillStyle = lookup[aaLookup(nt)];
                             } else {
                                 ctx.fillStyle = '#000000';
                             }
-                            ctx.fillText(c, 
-                                LEFT_PAD + (7 * labelMaxLength) + (8 * j),
-                                TOP_PAD + V_SPACE * (i + 1));
+                        }
+
+                        ctx.fillText(c,
+                            LEFT_PAD + middlePad + (CHAR_SPACE * i),
+                            TOP_PAD);
+                    });
+
+                    var i = 0;
+                    var diffs = {}
+                    angular.forEach(info.seqs, function(seq, id) {
+                        var label = seq.sample.id + ': ' + seq.seq_id;
+                        ctx.fillText(label, LEFT_PAD, TOP_PAD + V_PER_SEQ
+                            * (1 + i));
+
+                        angular.forEach(seq.sequence, function(c, j) {
+                            var left = LEFT_PAD + middlePad + (CHAR_SPACE * j);
+                            var top = TOP_PAD + V_PER_SEQ * (i + 1);
+                            if (j % 3 == 0) {
+                                var nt = seq.sequence.substring(j, j + 3);
+                                if (nt in aa_lookup) {
+                                    ctx.fillStyle = lookup[aaLookup(nt)];
+                                } else {
+                                    ctx.fillStyle = '#000000';
+                                }
+                            }
+
+                            // TODO: Compare with one change, not against
+                            // germline
+                            if (seq.sequence[j] != info.clone.germline[j]) {
+                                var aaStart = j - (j % 3);
+                                var gAA = aaLookup(info.clone.germline.substring(aaStart, aaStart
+                                    + 3));
+                                var sAA = aaLookup(seq.sequence.substring(aaStart, aaStart
+                                    + 3));
+                                ctx.beginPath();
+                                ctx.rect(left - 2.5, top - CHAR_SPACE + 8, 15, 15);
+                                ctx.lineWidth = 2;
+                                if (!(j in diffs)) {
+                                    diffs[j] = { 'silent': 0, 'change': 0,
+                                    'unk': 0 }
+                                }
+                                if (gAA == null || sAA == null) {
+                                    ctx.strokeStyle = '#0000ff';
+                                    diffs[j]['unk']++;
+                                } else if (gAA != sAA) {
+                                    ctx.strokeStyle = '#ff0000';
+                                    diffs[j]['change']++;
+                                } else if (gAA == sAA) {
+                                    ctx.strokeStyle = '#00ff00';
+                                    diffs[j]['silent']++;
+                                }
+                                ctx.stroke();
+                            }
+                            ctx.fillText(c, left, top);
                         });
                         i++;
                     });
+
+                    angular.forEach(diffs, function(vals, offset) {
+                        var t = '';
+                        if (vals['silent'] >= info.seqs.length / 2.0) {
+                            t = 'S';
+                            ctx.fillStyle = '#00ff00';
+                        } else if (vals['change'] >= info.seqs.length / 2.0) {
+                            t += (t.length > 0 ? '\n' : '') + 'N';
+                            ctx.fillStyle = '#ff0000';
+                        } else if (vals['unk'] >= info.seqs.length / 2.0) {
+                            t += (t.length > 0 ? '\n' : '') + 'U';
+                            ctx.fillStyle = '#0000ff';
+                        }
+                        ctx.fillText(t, LEFT_PAD + middlePad + offset *
+                            CHAR_SPACE, (1 + info.seqs.length) * V_PER_SEQ + 30);
+                    });
+                    drawRegion(ctx, 
+                        '#0000ff', 
+                        LEFT_PAD + middlePad,
+                        10,
+                        CHAR_SPACE,
+                        0,
+                        71,
+                        'FR1');
+                    drawRegion(ctx, 
+                        '#0000ff', 
+                        LEFT_PAD + middlePad,
+                        10,
+                        CHAR_SPACE,
+                        120,
+                        158,
+                        'FR2');
+                    drawRegion(ctx, 
+                        '#0000ff', 
+                        LEFT_PAD + middlePad,
+                        10,
+                        CHAR_SPACE,
+                        204,
+                        308,
+                        'FR3');
+
+                    drawRegion(ctx, 
+                        '#00ff00', 
+                        LEFT_PAD + middlePad,
+                        10,
+                        CHAR_SPACE,
+                        72,
+                        119,
+                        'CDR1');
+
+                    drawRegion(ctx, 
+                        '#00ff00', 
+                        LEFT_PAD + middlePad,
+                        10,
+                        CHAR_SPACE,
+                        159,
+                        203,
+                        'CDR2');
+
+                    drawRegion(ctx, 
+                        '#00ff00', 
+                        LEFT_PAD + middlePad,
+                        10,
+                        CHAR_SPACE,
+                        309,
+                        351,
+                        'CDR3');
+                }
+
+                var drawRegion = function(ctx, color, left, top, char_space, start, end, text) {
+                    ctx.fillText(text, left + start * char_space, 10);
+                    ctx.beginPath();
+                    ctx.strokeStyle = color;
+                    ctx.moveTo(left + start * char_space, top + 5);
+                    ctx.lineTo(left + end * char_space + 10, top + 5);
+                    ctx.stroke();
                 }
 
                 var init = function() {
