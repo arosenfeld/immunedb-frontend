b3ebe5642ba399dc55a78367a587bb09ef21993f
Aaron Rosenfeld <ar374@drexel.edu>
2015-02-10 11:04:28 -0500

    paging for modifications list

--- app/scripts/controllers/modification_log.js
+++ app/scripts/controllers/modification_log.js
@@ -1,19 +1,55 @@
 (function() {
     'use strict';
     angular.module('ImmunologyApp').controller('ModificationLogCtrl', ['$scope',
-            '$http', 'APIService', '$log',
-        function($scope, $http, APIService, $log) {
+            '$http', '$q', 'APIService', '$log',
+        function($scope, $http, $q, APIService, $log) {
+
+            $scope.prevPage = function() {
+                $scope.page = Math.max(1, $scope.page - 1);
+                $scope.pageable = false;
+                getModifications().then(function(result) {
+                    $scope.results = result;
+                    $scope.pageable = true;
+                });
+            }
+
+            $scope.nextPage = function() {
+                $scope.pageable = false;
+                $scope.page++;
+                getModifications().then(function(result) {
+                    $scope.results = result;
+                    $scope.pageable = true;
+                });
+            }
+
+            var getModifications = function() {
+                var def = $q.defer();
+                $http({
+                    method: 'GET',
+                    url: APIService.getUrl() + 'modification_log',
+                    params: {
+                        'page': $scope.page,
+                        'per_page': 10,
+                    }
+                }).success(function(data, status) {
+                    def.resolve(data['logs']);
+                }).error(function(data, status, headers, config) {
+                    def.reject();
+                });
+
+                return def.promise;
+            }
+
             var init = function() {
                 $scope.showLoader();
                 $scope.$parent.page_title = 'Modification Log';
-                $http({
-                    method: 'GET',
-                    url: APIService.getUrl() + 'modification_log'
-                }).success(function(data, status) {
-                    $scope.modifications = data['logs'];
+
+                $scope.pageable = false;
+                $scope.page = 1;
+                getModifications().then(function(result) {
+                    $scope.results = result;
+                    $scope.pageable = true;
                     $scope.hideLoader();
-                }).error(function(data, status, headers, config) {
-                    $scope.showError();
                 });
             }
 
